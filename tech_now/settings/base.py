663a9ec1ab7f0721c195053d78e2d4506f6dd20d
Jose Velasquez <jangel9829@gmail.com>
2022-01-24 00:03:40 -0400

    adding whitenoise

--- tech_now/settings/base.py
+++ tech_now/settings/base.py
@@ -85,6 +85,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
 
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
 ]
 
--- tech_now/settings/production.py
+++ tech_now/settings/production.py
@@ -8,11 +8,24 @@ SECRET_KEY = env['SECRET_KEY']
 
 DEBUG = False
 
-DATABASES['default'] = dj_database_url.config()
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
+COMPRESS_OFFLINE = True
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter',
+]
+COMPRESS_CSS_HASHING_METHOD = 'content'
 
+# Parse database configuration from $DATABASE_URL
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
 ALLOWED_HOSTS = ['*']
+
 
 try:
     from .local import *
