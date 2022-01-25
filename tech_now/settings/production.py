4e5495ca7b9dfb82771763c426ea3b64aecab2fe
Jose Velasquez <jangel9829@gmail.com>
2022-01-24 21:44:55 -0400

    Nice and running for heroku

--- tech_now/settings/production.py
+++ tech_now/settings/production.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 from .base import *
-#import dj_database_url
+import dj_database_url
 import os
 
 env = os.environ.copy()
@@ -18,7 +18,7 @@ COMPRESS_CSS_FILTERS = [
 COMPRESS_CSS_HASHING_METHOD = 'content'
 
 # Parse database configuration from $DATABASE_URL
-#DATABASES['default'] =  dj_database_url.config('sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3') )
+DATABASES['default'] =  dj_database_url.config(default='sqlite:////c:/xampp/htdocs/tech_now/db.sqlite3')
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
